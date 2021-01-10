b4727cd36f92fbc1921ab742fa6ce6a8970f3b46
EralpKocas <hkocas15@ku.edu.tr>
2021-01-10 23:11:52 +0300

    initial project is created

---
+++ KinectFusion/cmake-build-debug/CMakeFiles/KinectFusion.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/eralpkocas/Documents/TUM/3D Scanning & Motion Planning/Term Project/KinectFusion/FreeImageHelper.cpp" "/Users/eralpkocas/Documents/TUM/3D Scanning & Motion Planning/Term Project/KinectFusion/cmake-build-debug/CMakeFiles/KinectFusion.dir/FreeImageHelper.cpp.o"
+  "/Users/eralpkocas/Documents/TUM/3D Scanning & Motion Planning/Term Project/KinectFusion/main.cpp" "/Users/eralpkocas/Documents/TUM/3D Scanning & Motion Planning/Term Project/KinectFusion/cmake-build-debug/CMakeFiles/KinectFusion.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "CERES_EXPORT_INTERNAL_SYMBOLS"
+  "GFLAGS_IS_A_DLL=0"
+  "GOOGLE_GLOG_DLL_DECL="
+  "GOOGLE_GLOG_DLL_DECL_FOR_UNITTESTS="
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/Cellar"
+  "../../libs/Eigen"
+  "../"
+  "../../libs/Ceres/CMake/../include"
+  "../../libs/glog-lib/include"
+  "../../libs/Flann"
+  "/usr/local/include"
+  "/usr/local/include/eigen3"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ KinectFusion/cmake-build-debug/CMakeFiles/KinectFusion.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for KinectFusion.
+# This may be replaced when dependencies are built.
