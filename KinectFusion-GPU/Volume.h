f474704f1a9a9b2d44870eae1df383e699516612
mrahtapot <berke.atropat@tum.de>
2021-07-11 00:22:18 +0300

    Surface Prediction v2

---
+++ KinectFusion-GPU/Volume.h
@@ -0,0 +1,193 @@
+#pragma once
+
+#ifndef VOLUME_H
+#define VOLUME_H
+
+#include <limits>
+#include "Eigen.h"
+//#include "common.h"
+typedef unsigned int uint;
+
+struct Voxel
+{
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+    // position stored as 4 floats (4th component is supposed to be 1.0)
+    float tsdf_distance_value;
+    // color stored as 4 unsigned char
+    int tsdf_weight;
+    Vector4uc color;
+    bool is_occupied = false;
+};
+
+//! A regular volume dataset
+class Volume
+{
+public:
+
+    //! Initializes an empty volume dataset.
+    Volume(Vector3f min_, Vector3f max_, uint dx_ = 10, uint dy_ = 10, uint dz_ = 10, uint dim = 1);
+
+    ~Volume();
+
+    inline void computeMinMaxValues(double& minVal, double& maxVal) const
+    {
+        minVal = std::numeric_limits<double>::max();
+        maxVal = -minVal;
+        for (uint i1 = 0; i1 < dx*dy*dz; i1++)
+        {
+            if (minVal > vol[i1].tsdf_distance_value) minVal = vol[i1].tsdf_distance_value;
+            if (maxVal < vol[i1].tsdf_distance_value) maxVal = vol[i1].tsdf_distance_value;
+        }
+    }
+
+    //! Computes spacing in x,y,z-directions.
+    void compute_ddx_dddx();
+
+    //! Zeros out the memory
+    void zeroOutMemory();
+
+    //! Set the value at i.
+    inline void set(uint i, double val)
+    {
+        if (val > maxValue)
+            maxValue = val;
+
+        if (val < minValue)
+            minValue = val;
+
+        vol[i].tsdf_distance_value = (float) val;
+    }
+
+    //! Set the value at (x_, y_, z_).
+    inline void set(uint x_, uint y_, uint z_, Voxel val)
+    {
+        vol[getPosFromTuple(x_, y_, z_)] = val;
+    };
+
+    //! Get the value at (x_, y_, z_).
+    inline Voxel get(uint i) const
+    {
+        return vol[i];
+    };
+
+    //! Get the value at (x_, y_, z_).
+    inline Voxel get(uint x_, uint y_, uint z_) const
+    {
+        return vol[getPosFromTuple(x_, y_, z_)];
+    };
+
+    //! Get the value at (pos.x, pos.y, pos.z).
+    inline Voxel get(const Vector3i& pos_) const
+    {
+        return(get(pos_[0], pos_[1], pos_[2]));
+    }
+
+    //! Returns the cartesian x-coordinates of node (i,..).
+    inline double posX(int i) const
+    {
+        return min[0] + diag[0] * (double(i)*ddx);
+    }
+
+    //! Returns the cartesian y-coordinates of node (..,i,..).
+    inline double posY(int i) const
+    {
+        return min[1] + diag[1] * (double(i)*ddy);
+    }
+
+    //! Returns the cartesian z-coordinates of node (..,i).
+    inline double posZ(int i) const
+    {
+        return min[2] + diag[2] * (double(i)*ddz);
+    }
+
+    //! Returns the cartesian coordinates of node (i,j,k).
+    inline Vector3f pos(int i, int j, int k) const
+    {
+        Vector3f coord(0, 0, 0);
+
+        coord[0] = min[0] + (max[0] - min[0])*(double(i)*ddx);
+        coord[1] = min[1] + (max[1] - min[1])*(double(j)*ddy);
+        coord[2] = min[2] + (max[2] - min[2])*(double(k)*ddz);
+
+        return coord;
+    }
+
+    //! Returns the corresponding node of given cartesian coordinates.
+    inline Vector3f compute_grid(Vector3f p)
+    {
+        return Vector3f(((p[0] - min[0]) / (max[0] - min[0])) / ddx,
+                        ((p[1] - min[1]) / (max[1] - min[1])) / ddy,
+                        ((p[2] - min[2]) / (max[2] - min[2])) / ddz
+        );
+    }
+
+    inline Voxel set_occupied(Vector3f p){
+
+        Voxel voxel = get(((p[0] - min[0]) / (max[0] - min[0])) / ddx,
+                          ((p[1] - min[1]) / (max[1] - min[1])) / ddy,
+                          ((p[2] - min[2]) / (max[2] - min[2])) / ddz);
+        Voxel curr_voxel;
+        curr_voxel.tsdf_distance_value = voxel.tsdf_distance_value;
+        curr_voxel.tsdf_weight = voxel.tsdf_weight;
+        curr_voxel.color = voxel.color;
+        curr_voxel.is_occupied = true;
+        return curr_voxel;
+    }
+
+    //! Returns the Data.
+    Voxel* getData();
+
+    //! Sets all entries in the volume to '0'
+    void clean();
+
+    //! Returns number of cells in x-dir.
+    inline uint getDimX() const { return dx; }
+
+    //! Returns number of cells in y-dir.
+    inline uint getDimY() const { return dy; }
+
+    //! Returns number of cells in z-dir.
+    inline uint getDimZ() const { return dz; }
+
+    inline Vector3f getMin() { return min; }
+    inline Vector3f getMax() { return max; }
+
+    //! Sets minimum extension
+    void SetMin(Vector3f min_);
+
+    //! Sets maximum extension
+    void SetMax(Vector3f max_);
+
+    inline uint getPosFromTuple(int x, int y, int z) const
+    {
+        return x*dy*dz + y*dz + z;
+    }
+
+    //! VARIABLES
+
+    //! Lower left and Upper right corner.
+    Vector3f min, max;
+
+    //! max-min
+    Vector3f diag;
+
+    double ddx, ddy, ddz;
+    double dddx, dddy, dddz;
+
+    //! Number of cells in x, y and z-direction.
+    uint dx, dy, dz;
+
+    Voxel* vol;
+    double maxValue, minValue;
+    uint m_dim;
+
+private:
+
+    //! x,y,z access to vol*
+    inline Voxel vol_access(int x, int y, int z) const
+    {
+        return vol[getPosFromTuple(x, y, z)];
+    }
+};
+
+#endif // VOLUME_H
--- KinectFusion-GPU/surface_prediction.cu
+++ KinectFusion-GPU/surface_prediction.cu
@@ -1,77 +1,60 @@
 #include "data_types.h"
 #include "surface_prediction.h"
 
-__device__ Vector3f calculate_pixel_ray_cast(Vector3f pixel, Matrix3f rotation, Vector3f translation,
-                                 ImageConstants *&imageConstants, Vector3f eye, Volume *&global_volume,
-                                 Vector3f curr_grid) {
+//WARNING: Volume stuff might not have converted to CUDA right.
+
+__device__ Vector3f calculate_pixel_ray_cast(Matrix3f rotation, Vector3f translation, ImageConstants imageConstants) {
     int threadX = blockIdx.x * blockDim.x + threadIdx.x;
     int threadY = blockIdx.y * blockDim.y + threadIdx.y;
-    int threadZ = blockIdx.z * blockDim.z + threadIdx.z;
 
     //img to cam
     float camera_x = (threadX - imageConstants.cX) / imageConstants.fX;
     float camera_y = (threadY - imageConstants.cY) / imageConstants.fY;
     Vector3f camera_vec = Vector3f(camera_x, camera_y, 1.f);
     //cam to global
-    Vector3f raycast = rotation * camera_vec + translation;
+    Vector3f ray_cast = rotation * camera_vec + translation;
 
-    return raycast;
-
-    /*
-    //calculate_raycast_dir
-    Vector3f ray_dir = (raycast - eye).normalized();
-
-    //calculate_search_length
-    float t_x = (threadX - eye.x()) / ray_dir.x();
-    float t_y = (threadY - eye.y()) / ray_dir.y();
-    float t_z = (threadZ - eye.z()) / ray_dir.z();
-    float search_length = fmax(fmax(fabs(t_x), fabs(t_y)), fabs(t_z));
-
-    //place to gridInVolume
-    if (curr_grid.x() < 1 || curr_grid.x() >= global_volume.x - 1 ||
-        curr_grid.y() < 1 || curr_grid.y() >= global_volume.y - 1 ||
-        curr_grid.z() < 1 || curr_grid.z() >= global_volume.z - 1)
-        break;
-    */
+    return ray_cast;
 }
 
-__device__ Vector3f calculate_raycast_dir(Vector3f eye, Vector3f current_ray) {
+__device__ Vector3f calculate_ray_cast_dir(Vector3f eye, Vector3f current_ray) {
     return (current_ray - eye).normalized();
 }
 
-__device__ bool gridInVolume(ImageProperties *&image_properties, Volume *&global_volume, Vector3f curr_grid) {
-    int dx = global_volume->getDimX();
-    int dy = global_volume->getDimY();
-    int dz = global_volume->getDimZ();
-    return !(curr_grid.x() < 1 || curr_grid.x() >= global_volume.x - 1 ||
-             curr_grid.y() < 1 || curr_grid.y() >= global_volume.y - 1 ||
-             curr_grid.z() < 1 || curr_grid.z() >= global_volume.z - 1);
-}
-
-
 // P = O + t * R where R is normalized ray direction and O is eye, translation vector in our case.
 // Then, t = (P - O) / R
-__device__ float calculate_search_length(Vector3f eye, Vector3f pixel, Vector3f ray_dir) {
-    float t_x = (pixel.x() - eye.x()) / ray_dir.x();
-    float t_y = (pixel.y() - eye.y()) / ray_dir.y();
-    float t_z = (pixel.z() - eye.z()) / ray_dir.z();
+__device__ float calculate_search_length(Vector3f eye, Vector3f ray_dir) {
+    int threadX = blockIdx.x * blockDim.x + threadIdx.x;
+    int threadY = blockIdx.y * blockDim.y + threadIdx.y;
+    int threadZ = blockIdx.z * blockDim.z + threadIdx.z;
+    float t_x = (threadX - eye.x()) / ray_dir.x();
+    float t_y = (threadY - eye.y()) / ray_dir.y();
+    float t_z = (threadZ - eye.z()) / ray_dir.z();
 
     return fmax(fmax(fabs(t_x), fabs(t_y)), fabs(t_z));
 }
 
+__device__ bool gridInVolume(Volume* global_volume, Vector3f curr_grid) {
+    int dx = global_volume->getDimX();
+    int dy = global_volume->getDimY();
+    int dz = global_volume->getDimZ();
+    return !(curr_grid.x() < 1 || curr_grid.x() >= dx - 1 ||
+             curr_grid.y() < 1 || curr_grid.y() >= dy - 1 ||
+             curr_grid.z() < 1 || curr_grid.z() >= dz - 1);
+}
 
-__device__ float calculate_trilinear_interpolation(ImageConstants *&image_constants, GlobalVolume *&global_volume, Vector3f p) {
+__device__ float calculate_trilinear_interpolation(GlobalVolume* global_volume, Vector3f p) {
     Vector3i p_int = Vector3i((int) p.x(), (int) p.y(), (int) p.z());
 
-    //not sure if this is the right way to write it, had problem with the syntax lol
-    float c000 = global_volume->get(p_int.x(), p_int.y(), p_int.z()).TSDF_values;
-    float c001 = global_volume->get(p_int.x(), p_int.y(), p_int.z() + 1).TSDF_values;
-    float c010 = global_volume->get(p_int.x(), p_int.y() + 1, p_int.z()).TSDF_values;
-    float c011 = global_volume->get(p_int.x(), p_int.y() + 1, p_int.z() + 1).TSDF_values;
-    float c100 = global_volume->get(p_int.x() + 1, p_int.y(), p_int.z()).TSDF_values;
-    float c101 = global_volume->get(p_int.x() + 1, p_int.y(), p_int.z() + 1).TSDF_values;
-    float c110 = global_volume->get(p_int.x() + 1, p_int.y() + 1, p_int.z()).TSDF_values;
-    float c111 = global_volume->get(p_int.x() + 1, p_int.y() + 1, p_int.z() + 1).TSDF_values;
+    //couldn't find a way to do this one
+    float c000 = global_volume->get(p_int.x(), p_int.y(), p_int.z()).tsdf_distance_value;
+    float c001 = global_volume->get(p_int.x(), p_int.y(), p_int.z() + 1).tsdf_distance_value;
+    float c010 = global_volume->get(p_int.x(), p_int.y() + 1, p_int.z()).tsdf_distance_value;
+    float c011 = global_volume->get(p_int.x(), p_int.y() + 1, p_int.z() + 1).tsdf_distance_value;
+    float c100 = global_volume->get(p_int.x() + 1, p_int.y(), p_int.z()).tsdf_distance_value;
+    float c101 = global_volume->get(p_int.x() + 1, p_int.y(), p_int.z() + 1).tsdf_distance_value;
+    float c110 = global_volume->get(p_int.x() + 1, p_int.y() + 1, p_int.z()).tsdf_distance_value;
+    float c111 = global_volume->get(p_int.x() + 1, p_int.y() + 1, p_int.z() + 1).tsdf_distance_value;
 
     float xd = p.x() - p_int.x();
     float yd = p.y() - p_int.y();
@@ -90,97 +73,83 @@ __device__ float calculate_trilinear_interpolation(ImageConstants *&image_consta
     return c;
 }
 
-__global__ void helper_compute_normal_map(SurfaceLevelData *&surface_level_data) {
-    int curr_width = surface_level_data->level_img_width
-    int curr_height = surface_level_data->level_img_height
-    int numWH = curr_width * curr_height;
+__global__ void helper_compute_normal_map(int width, int height) {
+    int threadX = threadIdx.x + blockDim.x * blockIdx.x;
+    if (threadX >= width or threadX < 0)
+        return;
+    int threadY = threadIdx.y + blockDim.y * blockIdx.y;
+    if (threadY >= height or threadY < 0)
+        return;
 
-    for (int i = 1; i < curr_height - 1; i++) {
-        for (int j = 1; j < curr_width - 1; j++) {
-            Vector3f curr_vertex = surface_level_data->vertex_map_predicted[j + i * curr_width];
-            if (curr_vertex.z() == MINF || isnan(curr_vertex.z())) {
-                surface_level_data->normal_map_predicted[j + i * curr_width] = Vector3f(MINF, MINF, MINF);
-            } else {
-                int pixel_y = i;
-                int pixel_x = j;
+    Vector3f curr_vertex = vertex_map_predicted.ptr(threadY)[threadX];
 
-                int right_pixel = (pixel_x + 1) + pixel_y * curr_width;
-                int left_pixel = (pixel_x - 1) + pixel_y * curr_width;
-                int bottom_pixel = pixel_x + (pixel_y + 1) * curr_width;
-                int upper_pixel = pixel_x + (pixel_y - 1) * curr_width;
+    if (curr_vertex.z() == 0.f || isnan(curr_vertex.z())) {
+        normal_map_predicted.ptr(threadY)[threadX] = Vector3f(0.f, 0.f, 0.f);
+    } else {
+        Vector3f neigh_1 = Vector3f(vertex_map_predicted.ptr(threadY - 1)[threadX].x() -
+                                    vertex_map_predicted.ptr(threadY + 1)[threadX].x(),
+                                    vertex_map_predicted.ptr(threadY - 1)[threadX].y() -
+                                    vertex_map_predicted.ptr(threadY + 1)[threadX].y(),
+                                    vertex_map_predicted.ptr(threadY - 1)[threadX].z() -
+                                    vertex_map_predicted.ptr(threadY + 1)[threadX].z());
 
-                Vector3f neigh_1 = Vector3f(
-                        surface_level_data->vertex_map_predicted[left_pixel].x() -
-                        surface_level_data->vertex_map_predicted[right_pixel].x(),
-                        surface_level_data->vertex_map_predicted[left_pixel].y() -
-                        surface_level_data->vertex_map_predicted[right_pixel].y(),
-                        surface_level_data->vertex_map_predicted[left_pixel].z() -
-                        surface_level_data->vertex_map_predicted[right_pixel].z());
+        Vector3f neigh_2 = Vector3f(vertex_map_predicted.ptr(threadY)[threadX - 1].x() -
+                                    vertex_map_predicted.ptr(threadY)[threadX + 1].x(),
+                                    vertex_map_predicted.ptr(threadY)[threadX - 1].y() -
+                                    vertex_map_predicted.ptr(threadY)[threadX + 1].y(),
+                                    vertex_map_predicted.ptr(threadY)[threadX - 1].z() -
+                                    vertex_map_predicted.ptr(threadY)[threadX + 1].z());
 
-                Vector3f neigh_2 = Vector3f(
-                        surface_level_data->vertex_map_predicted[upper_pixel].x() -
-                        surface_level_data->vertex_map_predicted[bottom_pixel].x(),
-                        surface_level_data->vertex_map_predicted[upper_pixel].y() -
-                        surface_level_data->vertex_map_predicted[bottom_pixel].y(),
-                        surface_level_data->vertex_map_predicted[upper_pixel].z() -
-                        surface_level_data->vertex_map_predicted[bottom_pixel].z());
-
-                Vector3f cross_prod = neigh_1.cross(neigh_2);
-                cross_prod.normalize();
-                if (cross_prod.z() > 0) cross_prod *= -1;
-
-                surface_level_data->normal_map_predicted[j + i * curr_width] = cross_prod;
-            }
-        }
+        Vector3f cross_prod = neigh_1.cross(neigh_2);
+        cross_prod.normalize();
+        if (cross_prod.z() > 0) cross_prod *= -1;
+        normal_map_predicted.ptr(threadY)[threadX] = cross_prod;
     }
 }
 
-__global__ void predict_surface(ImageConstants *&image_constants, SurfaceLevelData *&surface_level_date, GlobalVolume *&global_volume) {
+__global__ void predict_surface(ImageConstants image_constants, SurfaceLevelData* surf_data, GlobalVolume* global_volume,
+                                Matrix3f rotation, Vector3f translation, int width, int height, int level) {
     for (int level = 0; level < image_constants->num_levels; level++) {
-        Vector3f translation = get_translation(image_constants);
-        cv::cuda::GpuMat rotation = get_rotation(image_constants);
+
+        int threadX = threadIdx.x + blockDim.x * blockIdx.x;
+        if (threadX >= width or threadX < 0)
+            return;
+
+        int threadY = threadIdx.y + blockDim.y * blockIdx.y;
+        if (threadY >= height or threadY < 0)
+            return;
 
         float step_size = image_constants->truncation_distance;
-        int width = (int) surface_level_date->level_img_width;
-        int height = (int) surface_level_date->level_img_height;
-        for (int j = 0; j < height; j++) {
-            for (int i = 0; i < width; i++) {
-                // +0.5 for reaching pixel centers
-                Vector3f pixel_ray = calculate_pixel_raycast(Vector3f(float(i + 0.5), float(j + 0.5), 1.f),
-                                                             rotation, translation,
-                                                             surface_level_data.level_fX,
-                                                             surface_level_data.level_fY,
-                                                             surface_level_data.level_cX,
-                                                             surface_level_data.level_cY);
-                float camera_x = ((float) (i + 0.5) - surface_level_data.level_cX) /
-                                 surface_level_data.level_fX;  // image to camera
-                float camera_y = ((float) (j + 0.5) - surface_level_data.level_cY) /
-                                 surface_level_data.level_fY;  // image to camera
+
+        // +0.5 for reaching pixel centers
+        Vector3f pixel_ray = calculate_pixel_raycast(rotation, translation, image_constants);
+                float camera_x = ((float) (threadX + 0.5) - surf_data.level_cX) / surf_data.level_fX;  // image to camera
+                float camera_y = ((float) (threadY + 0.5) - surf_data.level_cY) / surf_data.level_fY;  // image to camera
+
                 Vector3f pixel = Vector3f(camera_x, camera_y, 1.f);
                 Vector3f ray_dir_2 = (rotation * pixel).normalized();
                 Vector3f ray_dir = calculate_raycast_dir(translation, pixel_ray);
 
                 //float t = calculate_search_length(translation, pixel_ray, ray_dir);  // t
-                float max_ray_length = Vector3i(global_volume.TSDF_values[0],
-                                                global_volume.TSDF_values[1],
-                                                global_volume.TSDF_values[2].norm();
+                float max_ray_length = Vector3i(global_volume->getDimX(),
+                                                global_volume->getDimY(),
+                                                global_volume->getDimZ()).norm();
 
                 //Vector3f pixel_grid = global_volume->compute_grid(pixel_ray);
                 //Vector3f ray_dir_grid = global_volume->compute_grid(ray_dir);
 
-                //TODO: I AM HERE
-                Vector3f init_pos = Vector3f(MINF, MINF, MINF);
+                Vector3f init_pos = Vector3f(0.f, 0.f, 0.f);
                 for (float step = 0; step < max_ray_length; step += step_size * 0.5) {
                     Vector3f curr_pos = translation + (float) step * ray_dir;
                     Vector3f curr_grid = global_volume->compute_grid(curr_pos);
 
-                    if (!gridInVolume(image_properties, global_volume, curr_grid)) continue;
+                    if (!gridInVolume(global_volume, curr_grid)) continue;
                     init_pos = curr_pos;
                     break;
                 }
 
-                if(!init_pos.allFinite() || init_pos.x() == MINF ||
-                   init_pos.y() == MINF || init_pos.z() == MINF) continue;
+                if(!init_pos.allFinite() || init_pos.x() == 0.f ||
+                init_pos.y() == 0.f || init_pos.z() == 0.f) continue;
 
                 Vector3f eye_grid = global_volume->compute_grid(init_pos);
                 float tsdf = global_volume->
@@ -202,15 +171,12 @@ __global__ void predict_surface(ImageConstants *&image_constants, SurfaceLevelDa
                     if (prev_tsdf < 0.f && curr_tsdf > 0.f) break;  // zero-crossing from behind
 
                     if (prev_tsdf > 0.f && curr_tsdf < 0.f)  // zero-crossing is found
-                    {
-                        float prev_tri_interpolated_sdf = calculate_trilinear_interpolation(image_properties,
-                                                                                            global_volume,
+                        {
+                        float prev_tri_interpolated_sdf = calculate_trilinear_interpolation(global_volume,
                                                                                             prev_grid);
-                        float curr_tri_interpolated_sdf = calculate_trilinear_interpolation(image_properties,
-                                                                                            global_volume,
+                        float curr_tri_interpolated_sdf = calculate_trilinear_interpolation(global_volume,
                                                                                             curr_grid);
 
-                        //TODO: CHECK HERE
                         Voxel before = global_volume->get(curr_grid.x(), curr_grid.y(), curr_grid.z());
                         global_volume->set(translation.x(), translation.y(), translation.z(),
                                            global_volume->set_occupied(curr_grid));
@@ -220,22 +186,20 @@ __global__ void predict_surface(ImageConstants *&image_constants, SurfaceLevelDa
                         // t_star = t - ((step_size * prev_tsdf) / (curr_tsdf - prev_tsdf))
 
                         float t_star = step - ((step_size * 0.5f * prev_tri_interpolated_sdf)
-                                               / (curr_tri_interpolated_sdf - prev_tri_interpolated_sdf));
+                                / (curr_tri_interpolated_sdf - prev_tri_interpolated_sdf));
 
                         Vector3f grid_location = translation + t_star * ray_dir;
 
-                        if (!gridInVolume(image_constants, global_volume, grid_location)) break;
+                        if (!gridInVolume(global_volume, grid_location)) break;
 
                         Vector3f vertex = translation + t_star * ray_dir;
 
-                        surface_level_data->vertex_map_predicted[j * width + i] = vertex;
-                        //image_properties->all_data[level].normal_map_predicted[j*width + i] = normal;
-                    }
+                        vertex_map_predicted.ptr(threadY)[threadX] = vertex;
+                        }
                     prev_tsdf = curr_tsdf;
                     prev_grid = curr_grid;
                 }
-            }
-        }
-        helper_compute_normal_map(image_constants, level);
+        helper_compute_normal_map(width, height);
     }
+}
 }
